b8155335466be6bb0302ff4722002a7352460c13
Alessandro Chiozza <chiozzalessandro@gmail.com>
2022-04-02 13:12:38 +0200

    Add documentetion for ResourceManager.h

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -18,12 +18,36 @@ using std::shared_ptr;
 
 namespace ResourceManager
 {
+	/** 
+	 * @brief Function to get a texture from loaded textures
+	 * @param key texture name
+	 * @return Const reference to the texture 
+	*/
 	const Texture & texture(const string & key);
+
+	/** 
+	 * @brief Function to get an image from loaded images
+	 * @param key image name
+	 * @return Const reference to the image
+	*/
 	const Image & image(const string & key);
+
+	/** 
+	 * @brief Function to get a music from loaded musics
+	 * @param key music name
+	 * @return Pointer to the music
+	*/
 	const shared_ptr<Music> music(const string & key);
+
+	/** 
+	 * @brief Function to get a font from loaded font
+	 * @param key font name
+	 * @return Const reference to the font
+	*/
 	const Font & font(const string & key);
 
+	/// @brief Function to init resources
 	void init();
 };
 
-#endif
+#endif // RESOURCEMANAGER_H
